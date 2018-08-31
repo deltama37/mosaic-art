265638c151142291cf25231ffea47305823e6e70
Ryota Misumi <3delta7@gmail.com>
2018-08-31 20:54:18 +0900

    修正追加

--- src/download.sh
+++ src/download.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+pushd `dirname $0`
+
 if [ -z $1 ]; then
   echo "please input keyword"
+  exit 1
 fi
 
 OUTPUT_DIR="../photos"
@@ -9,7 +12,7 @@ KEYWORD=$1
 
 googleimagesdownload --keywords "${KEYWORD}" --output_directory="${OUTPUT_DIR}" --no_directory --prefix="${KEYWORD}" --limit 5 --aspect_ratio=square
 
-pushd "${OUTPUT_DIR}"
+cd "${OUTPUT_DIR}"
 
 i=0
 for file in *; do
